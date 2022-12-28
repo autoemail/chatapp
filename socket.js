0069cab6abbd020aed515c9e1953568827b53c63
Devraj Ash <65898509+autoemail@users.noreply.github.com>
2022-12-28 23:46:10 +0530

    first commit

---
+++ socket.js
@@ -0,0 +1,24 @@
+const users = [];
+
+const onSocket = (io) => {
+	io.on("connection", (socket) => {
+		socket.on("user:join", (name) => {
+			!users.some((user) => user.name === name) &&
+				users.push({ name, sockeId: socket.id });
+			io.emit("global:message", `${name} just joined !`);
+		});
+
+		socket.on("message:send", (payload) => {
+			socket.broadcast.emit("message:receive", payload);
+		});
+
+		socket.on("disconnect", () => {
+            try{
+			const user = users.filter((user) => user.sockeId === socket.id);
+			io.emit("global:message", `${user[0]?.name} just left !`);
+            }catch{}
+		});
+	});
+};
+
+export default onSocket;
